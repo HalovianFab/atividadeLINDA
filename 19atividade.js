aa917390fe176f2d4780c564ee7c69f9c542f1cb
HalovianFab <bernardescatarina31@gmail.com>
2026-08-28 10:13:02 -0400

    Add files via upload

---
+++ 19atividade.js
@@ -0,0 +1,16 @@
+// 1. Imprimir todos os números pares entre 1 e 100
+
+for (let i = 1; i <= 100; i++) {
+    if (i % 2 == 0) {
+        console.log(i)
+    }
+}
+
+
+// 2. Imprimir todos os anos pares entre 2000 e 2026
+
+for (let ano = 2000; ano <= 2026; ano++) {
+    if (ano % 2 == 0) {
+        console.log(ano)
+    }
+}
